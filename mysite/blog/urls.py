3c37ca5d992bf24e0b11279db572674c9f1d4fa7
jorgesmunoz <jorgesmunoz@gmail.com>
2022-04-08 11:11:15 -0300

    part five

---
+++ mysite/blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from blog import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('post/</pk>', views.DetailView.as_view(), name='post_detail'),
+    path('post/new/', views.CreatePostView.as_view(), name='post_new'),
+    path('post/</pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('post/</pk>/remove/', views.PostDeleteView.as_view(), name='post_remove'),
+    path('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
+]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,78 @@
-from django.shortcuts import render
-from django.views.generic import (TemplateView)
+from distutils.log import Log
+from time import timezone
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from django.urls import reverse_lazy
+from django.views.generic import (
+    TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from blog.models import Post, Comment
+from blog.forms import PostForm, CommentForm
 
 # Create your views here.
 
 
 class AboutView(TemplateView):
     template_name = 'about_html'
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        # This an SQL sentence
+        # https://docs.djangoproject.com/en/4.0/ref/models/querysets/#field-lookups-1
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    login_url = '/login'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = '/login'
+    redirect_field_name = 'blog/post_detail.html'
+    model = Post
+
+    # Not published post list
+    def get_queryset(self):
+        # This is an SQL sentence
+        # https://docs.djangoproject.com/en/4.0/ref/models/querysets/#field-lookups-1
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, )
